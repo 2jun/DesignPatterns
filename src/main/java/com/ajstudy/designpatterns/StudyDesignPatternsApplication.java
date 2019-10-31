308d74d46ff97c7651187d22ae400fb21ae14c8d
张爱俊 <724755391@qq,com>
2019-10-31 16:47:23 +0800

    设计模式Demo项目

---
+++ src/main/java/com/ajstudy/designpatterns/StudyDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.ajstudy.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDesignPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajstudy/designpatterns/studydesignpatterns/StudyDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajstudy.designpatterns.studydesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
